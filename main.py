b102ca0df86a043b735f2f6b6c0c28296a8a4a24
mr008 <976728139@qq.com>
2020-04-30 21:53:54 -0700

    it can take file input now

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #Ok! Wish us good luck!
+import sys
 def main() -> None:
-    ...  # program execution begins here
-
+    #config_address=str(sys.argv[1])
+    config_address="E:\\coding programs\\ConnectN\\config_files\\3X3X3.txt"
+    board_config = {}
+    with open(config_address) as ca:
+        for line in ca:
+            key, index = line.strip().split(':')
+            board_config[key.strip()] = index.strip()
+    blank_char=board_config["blank_char"]
+    num_rows = board_config["num_rows"]
+    num_pieces_to_win = board_config["num_pieces_to_win"]
+    num_cols = board_config["num_cols"]
 
 if __name__ == '__main__':
     main()
