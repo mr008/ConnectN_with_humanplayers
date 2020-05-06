e43850f2835123442fc008b364f9521c2f76eae2
mr008 <976728139@qq.com>
2020-05-05 18:30:12 -0700

    added config file

--- ConnectNGame/src/game.py
+++ ConnectNGame/src/game.py
@@ -149,19 +149,7 @@ class Game(object):
             return False
 
 
-    def get_from_file(self,config_address):
-        # config_address=str(sys.argv[1])
-        #config_address = "E:\\coding programs\\ConnectN\\config_files\\3X3X3.txt"
-        board_config = {}
-        with open(config_address) as ca:
-            for line in ca:
-                key, index = line.strip().split(':')
-                board_config[key.strip()] = index.strip()
-        blank_char = board_config["blank_char"]
-        num_rows = board_config["num_rows"]
-        num_pieces_to_win = board_config["num_pieces_to_win"]
-        num_cols = board_config["num_cols"]
-        return Game(blank_char,num_rows,num_pieces_to_win,num_cols)
+
 
     def start_game(self):
         self.setup_players()
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #Ok! Wish us good luck!
 import sys
 from ConnectNGame.src.game import Game
+from ConnectNGame.src.config import Config
+
 
 def main() -> None:
+
+    config1=Config()
     config_address=str(sys.argv[1])
-    game1=Game()
-    game1 = Game.get_from_file(config_address)
+    game1=config1.get_from_file(config_address)
     game1.setup_players()
     game1.start_game()
 
