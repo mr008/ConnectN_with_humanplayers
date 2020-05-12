90badc9deac49ebfb415343f860544e26d448746
acmoats <acmoats@ucdavis.edu>
2020-05-11 21:52:04 -0700

    work on test cases

--- ConnectNGame/test/test_board.py
+++ ConnectNGame/test/test_board.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from .print_capturer import PrintCapturer
+#from .print_capturer import PrintCapturer
 from ConnectNGame.src.board import Board
 
 class TestBoard(unittest.TestCase):
@@ -12,9 +12,21 @@ class TestBoard(unittest.TestCase):
 
     def test_check_full(self):
         board1 = Board(3, 3, "*")
-        board1.my_list = ("***", "*", "**")
-        bool =board1.check_full(1)
+        for i in range(2):
+            board1.my_list[i][1] = '^'
+        bool = board1.check_full(1)
         self.assertEqual(bool, False)
+        board2 = Board(6,6,"&")
+        for j in range(6):
+            board2.my_list[j][3] = "%"
+        check = board2.check_full(3)
+        self.assertEqual(True,check)
+        board3 = Board(5,4,'^')
+        for k in range(5):
+            board3.my_list[k][0] = '*'
+        check2 = board3.check_full(0)
+        self.assertEqual(check2, True)
+
 
 
 
