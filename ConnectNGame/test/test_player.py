89d64bccd95279ba573964cdedf8cd72eb8e98a3
mr008 <976728139@qq.com>
2020-05-11 21:41:58 -0700

    More test on test players

--- ConnectNGame/test/test_player.py
+++ ConnectNGame/test/test_player.py
@@ -7,9 +7,21 @@ class TestPlayer(unittest.TestCase):
         my_player = Player('sam', '?')
         self.assertEqual(my_player.get_name(),'sam')
 
+        my_player = Player('', '?')
+        self.assertEqual(my_player.get_name(), '')
+
+        my_player = Player('ALFJDSLFJADSLKFJADSKLFJADSJFDASKLFJDASKJFLADSJFDASLKFJADSKFJ', '%')
+        self.assertEqual(my_player.get_name(), 'ALFJDSLFJADSLKFJADSKLFJADSJFDASKLFJDASKJFLADSJFDASLKFJADSKFJ')
+
     def test_get_piece(self):
         my_player = Player('sam', '?')
         self.assertEqual(my_player.get_piece(),'?')
+
+        my_player = Player('1', '!')
+        self.assertEqual(my_player.get_piece(), '!')
+
+        my_player = Player('SFAS', '')
+        self.assertEqual(my_player.get_piece(), '')
 
 
 if __name__ == '__main__':
